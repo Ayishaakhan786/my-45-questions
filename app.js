2601105cc4b28ca8e36b382bc7d73b0dad9e9eba
Ayishaakhan786 <160420762+Ayishaakhan786@users.noreply.github.com>
2024-02-28 14:16:38 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+var qoute = "A person who never made a mistake never tried anything new.”";
+var author = "Albert Einstein";
+console.log("".concat(author, " once said,\"").concat(qoute));
---
+++ app.ts
@@ -0,0 +1,3 @@
+let qoute: string= "A person who never made a mistake never tried anything new.”";
+let author: string= "Albert Einstein";
+console.log(`${author} once said,"${qoute}`);
