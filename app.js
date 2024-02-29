cebedfb8d93028a996fe2d04ab17905b35bca151
Ayishaakhan786 <160420762+Ayishaakhan786@users.noreply.github.com>
2024-02-29 13:30:29 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-var favoriteNumber = 4;
-console.log("My favorite number is ".concat(favoriteNumber));
+console.log(5 + 3);
+console.log(11 - 3);
+console.log(4 * 2);
+console.log(16 / 2);
--- app.ts
+++ app.ts
@@ -1,2 +1,4 @@
-let favoriteNumber: number = 4;
-console.log(`My favorite number is ${favoriteNumber}`);
+console.log(5 + 3);
+console.log(11 - 3);
+console.log(4 * 2);
+console.log(16 / 2);
