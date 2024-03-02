00380c6fce05dd0a3b930ed1b81018b995a1ff89
Ayishaakhan786 <160420762+Ayishaakhan786@users.noreply.github.com>
2024-03-02 14:21:09 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-// Exercise 14
+// Exercise 16
 var guest_list = ['Tania', 'Esha', 'Rida', 'Saima'];
+// for (var i = 0; i < guest_list.length; i++) {
+//     console.log('Respected Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank you.\n');
+// }
+var not_present = 'Esha';
+var new_guest = 'Ahsan';
+guest_list[1] = new_guest;
 for (var i = 0; i < guest_list.length; i++) {
-    console.log('Respected Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank you.');
+    console.log('Respected Madam ' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank you.\n');
+}
+// console.log("Mrs. ".concat(not_present, " will not coming tomorrow on dinner."));
+guest_list.unshift('Javeria', 'Hina', 'Sana');
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected Madam ' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank you.\n');
 }
