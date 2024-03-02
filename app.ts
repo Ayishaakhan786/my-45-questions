dbf6e6d95c9fbe375d67acfd73ee7a50dfbe3233
Ayishaakhan786 <160420762+Ayishaakhan786@users.noreply.github.com>
2024-03-02 14:21:53 -0800

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,16 +1,12 @@
-// Exercise 16
+// Exercise 15 
 var guest_list = ['Tania', 'Esha', 'Rida', 'Saima'];
-// for (var i = 0; i < guest_list.length; i++) {
-//     console.log('Respected Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank you.\n');
-// }
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank you.\n');
+}
 var not_present = 'Esha';
 var new_guest = 'Ahsan';
 guest_list[1] = new_guest;
 for (var i = 0; i < guest_list.length; i++) {
-    console.log('Respected Madam ' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank you.\n');
+    console.log('Respected Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank you.\n');
 }
-// console.log("Mrs. ".concat(not_present, " will not coming tomorrow on dinner."));
-guest_list.unshift('Javeria','Hina','Sana');
-for (var i = 0; i < guest_list.length; i++) {
-    console.log('Respected Madam ' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank you.\n');
-}
+console.log("Mrs. ".concat(not_present, " will not coming tomorrow on dinner."));
