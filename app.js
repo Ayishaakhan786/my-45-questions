83597681716b3164ff7366299cf5563b7ac7eb27
Ayishaakhan786 <160420762+Ayishaakhan786@users.noreply.github.com>
2024-03-02 14:52:44 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
-// Exercise 17 
-var guest_list = ['Tania', 'Esha', 'Rida', 'Saima'];
-// for (var i = 0; i < guest_list.length; i++) {
-//     console.log('Respected Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank you.\n');
-// }
-var not_present = 'Esha';
-var new_guest = 'Ahsan';
-guest_list[1] = new_guest;
-// for (var i = 0; i < guest_list.length; i++) {
-//     console.log('Respected Madam ' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank you.\n');
-// }
-// console.log("Mrs. ".concat(not_present, " will not coming tomorrow on dinner."));
-guest_list.unshift('Javeria', 'Hina', 'Sana');
-// for (var i = 0; i < guest_list.length; i++) {
-//     console.log('Respected Madam ' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank you.\n');
-// }
-console.log('\nunfortunately we can not arrange a big table so, only two people allow');
-while (guest_list.length > 2) {
-    var remove_guest = guest_list.pop();
-    console.log("sorry Madam. ".concat(remove_guest, " you are not invited."));
-}
-for (var i = 0; i < guest_list.length; i++) {
-    console.log('Respected Madam ' + guest_list[i] + ',\nyes you are still invited on tomorrow\nThank you.\n');
-}
-guest_list.splice(0, 2);
-console.log(guest_list);
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+// Exercise 18
+var places = ['Cape Town', 'Texas', 'Lahore', 'Karachi', 'New york'];
+console.log('original ' + places);
+console.log('copy ' + __spreadArray([], places, true).sort());
+console.log('original ' + places);
+console.log('copy ' + __spreadArray([], places, true).sort().reverse());
+console.log('copy ' + __spreadArray([], places, true).sort());
+console.log('copy ' + __spreadArray([], places, true).sort().reverse());
--- app.ts
+++ app.ts
@@ -1,26 +1,8 @@
-// Exercise 17 
-var guest_list = ['Tania', 'Esha', 'Rida', 'Saima'];
-// for (var i = 0; i < guest_list.length; i++) {
-//     console.log('Respected Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank you.\n');
-// }
-var not_present = 'Esha';
-var new_guest = 'Ahsan';
-guest_list[1] = new_guest;
-// for (var i = 0; i < guest_list.length; i++) {
-//     console.log('Respected Madam ' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank you.\n');
-// }
-// console.log("Mrs. ".concat(not_present, " will not coming tomorrow on dinner."));
-guest_list.unshift('Javeria', 'Hina', 'Sana');
-// for (var i = 0; i < guest_list.length; i++) {
-//     console.log('Respected Madam ' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank you.\n');
-// }
-console.log('\nunfortunately we can not arrange a big table so, only two people allow');
-while(guest_list.length>2){
-    let remove_guest = guest_list.pop();
-    console.log(`sorry Madam. ${remove_guest} you are not invited.`);
-}
-for (var i = 0; i < guest_list.length; i++) {
-    console.log('Respected Madam ' + guest_list[i] + ',\nyes you are still invited on tomorrow\nThank you.\n');
-}
-guest_list.splice(0,2)
-console.log(guest_list)
+// Exercise 18
+let places: string [] = ['Cape Town','Texas','Lahore','Karachi','New york'];
+console.log('original ' + places);
+console.log('copy ' + [...places].sort());
+console.log('original ' + places);
+console.log('copy ' + [...places].sort().reverse());
+console.log('copy ' + [...places].sort());
+console.log('copy ' + [...places].sort().reverse());
