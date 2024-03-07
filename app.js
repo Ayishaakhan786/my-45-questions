2cb3e63fcda710edeff2a7a4132bc43a0aeb614c
Ayishaakhan786 <160420762+Ayishaakhan786@users.noreply.github.com>
2024-03-07 15:51:11 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-// Exercise 18
-var places = ['Cape Town', 'Texas', 'Lahore', 'Karachi', 'New york'];
-console.log('original ' + places);
-console.log('copy ' + __spreadArray([], places, true).sort());
-console.log('original ' + places);
-console.log('copy ' + __spreadArray([], places, true).sort().reverse());
-console.log('copy ' + __spreadArray([], places, true).sort());
-console.log('copy ' + __spreadArray([], places, true).sort().reverse());
+// Exercise 23
+var cars = 'Subaru';
+// Test 1: Equality comparison(True)
+console.log("is car == 'Subaru'? I predict true.");
+console.log(cars == 'Subaru'); // True 
+// Test 2: strict equality comparison (True)
+console.log("Is car === 'Subaru'? I predict ture.");
+console.log(cars === 'Subaru'); // True
+// Test 3: Enequality comparison (False)
+console.log("is car != 'Subaru'? I predict fasle.");
+console.log(cars != 'Subaru'); // False
+// Test 4: strict Enequality comparison (False)
+console.log("is car !== 'Subaru'? I predict fasle.");
+console.log(cars !== 'Subaru'); // False
+// Test 5: Less than comparison (False)
+console.log("is car < 'Subaru'? I predict fasle.");
+console.log(cars < 'Subaru'); // False (lexicographic compariosn)
+// Test 6: Greater than comparison (False)
+console.log("is car > 'Subaru'? I predict fasle.");
+console.log(cars > 'Subaru'); // False (lexicographic compariosn)
+// Test 7: Less than or equal comparison (True)
+console.log("is car <= 'Subaru'? I predict true.");
+console.log(cars <= 'Subaru'); // True
+// Test 8: Greater than or equal comparison (True)
+console.log("is car >= 'Subaru'? I predict true.");
+console.log(cars >= 'Subaru'); // True 
+// Test 9: Chacking truthness (True)
+console.log("Is car? I predict true.");
+console.log(cars); // True (non-empty sting is truthy)
+// Test 10:  Chacking Falseness (False)
+console.log("Is car? I predict false.");
+console.log(!cars); // false (negation of a truthy value)
--- app.ts
+++ app.ts
@@ -1,8 +1,32 @@
-// Exercise 18
-let places: string [] = ['Cape Town','Texas','Lahore','Karachi','New york'];
-console.log('original ' + places);
-console.log('copy ' + [...places].sort());
-console.log('original ' + places);
-console.log('copy ' + [...places].sort().reverse());
-console.log('copy ' + [...places].sort());
-console.log('copy ' + [...places].sort().reverse());
+// Exercise 23
+let cars : string ='Subaru';
+// Test 1: Equality comparison(True)
+console.log("is car == 'Subaru'? I predict true.");
+console.log(cars == 'Subaru'); // True 
+// Test 2: strict equality comparison (True)
+console.log("Is car === 'Subaru'? I predict ture.");
+console.log(cars === 'Subaru'); // True
+// Test 3: Enequality comparison (False)
+console.log("is car != 'Subaru'? I predict fasle.");
+console.log(cars != 'Subaru'); // False
+// Test 4: strict Enequality comparison (False)
+console.log("is car !== 'Subaru'? I predict fasle.");
+console.log(cars !== 'Subaru'); // False
+// Test 5: Less than comparison (False)
+console.log("is car < 'Subaru'? I predict fasle.");
+console.log(cars < 'Subaru'); // False (lexicographic compariosn)
+// Test 6: Greater than comparison (False)
+console.log("is car > 'Subaru'? I predict fasle.");
+console.log(cars > 'Subaru'); // False (lexicographic compariosn)
+// Test 7: Less than or equal comparison (True)
+console.log("is car <= 'Subaru'? I predict true.");
+console.log(cars <= 'Subaru'); // True
+// Test 8: Greater than or equal comparison (True)
+console.log("is car >= 'Subaru'? I predict true.");
+console.log(cars >= 'Subaru'); // True 
+// Test 9: Chacking truthness (True)
+console.log("Is car? I predict true.");
+console.log(cars); // True (non-empty sting is truthy)
+// Test 10:  Chacking Falseness (False)
+console.log("Is car? I predict false.");
+console.log(!cars); // false (negation of a truthy value)
