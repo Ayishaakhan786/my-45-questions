c3525f0ada236f486a323810b6d3b87d92643af5
Ayishaakhan786 <160420762+Ayishaakhan786@users.noreply.github.com>
2024-03-07 02:40:23 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Exercise 19
+// var guest_list = ['Tania', 'Esha', 'Rida', 'Saima'];
+// for (var i = 0; i < guest_list.length; i++) {
+//     console.log('Respected Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank you.\n');
+// }
+// export{guest_list}
+var app_1 = require("../Guest List/app");
+console.log("n PRINTING NUMBER OF GUESTS INVITED");
+console.log("we had finally invited ".concat(app_1.guest_list.length, " from exercise 14"));
---
+++ main.ts
@@ -0,0 +1,9 @@
+// Exercise 19
+// var guest_list = ['Tania', 'Esha', 'Rida', 'Saima'];
+// for (var i = 0; i < guest_list.length; i++) {
+//     console.log('Respected Madam' + guest_list[i] + ',\nWe invited you on dinner tomorrow.\n\nThank you.\n');
+// }
+// export{guest_list}
+import{ guest_list } from '../Guest List/app'
+console.log(`n\ PRINTING NUMBER OF GUESTS INVITED`)
+console.log(`we had finally invited ${guest_list.length} from exercise 14`)
