8b0c0b0f6ca5ee3196c3a702ba749ca077fd055e
Ayishaakhan786 <160420762+Ayishaakhan786@users.noreply.github.com>
2024-03-07 15:51:51 -0800

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,32 +1,6 @@
-// Exercise 23
-let cars : string ='Subaru';
-// Test 1: Equality comparison(True)
-console.log("is car == 'Subaru'? I predict true.");
-console.log(cars == 'Subaru'); // True 
-// Test 2: strict equality comparison (True)
-console.log("Is car === 'Subaru'? I predict ture.");
-console.log(cars === 'Subaru'); // True
-// Test 3: Enequality comparison (False)
-console.log("is car != 'Subaru'? I predict fasle.");
-console.log(cars != 'Subaru'); // False
-// Test 4: strict Enequality comparison (False)
-console.log("is car !== 'Subaru'? I predict fasle.");
-console.log(cars !== 'Subaru'); // False
-// Test 5: Less than comparison (False)
-console.log("is car < 'Subaru'? I predict fasle.");
-console.log(cars < 'Subaru'); // False (lexicographic compariosn)
-// Test 6: Greater than comparison (False)
-console.log("is car > 'Subaru'? I predict fasle.");
-console.log(cars > 'Subaru'); // False (lexicographic compariosn)
-// Test 7: Less than or equal comparison (True)
-console.log("is car <= 'Subaru'? I predict true.");
-console.log(cars <= 'Subaru'); // True
-// Test 8: Greater than or equal comparison (True)
-console.log("is car >= 'Subaru'? I predict true.");
-console.log(cars >= 'Subaru'); // True 
-// Test 9: Chacking truthness (True)
-console.log("Is car? I predict true.");
-console.log(cars); // True (non-empty sting is truthy)
-// Test 10:  Chacking Falseness (False)
-console.log("Is car? I predict false.");
-console.log(!cars); // false (negation of a truthy value)
+// Exercise 22
+//let array: (string | number) [] = ['Ayisha','Sameer', 25 , 6 , 22 ];
+//console.log(array[5]);
+// correction 
+let array: (string | number) [] = ['Ayisha','Sameer', 25 , 6 , 22 ];
+console.log(array[4]);
