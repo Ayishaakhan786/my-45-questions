26b1ded7e7d51da74a8875740dbc49af6b7ea28d
Ayishaakhan786 <160420762+Ayishaakhan786@users.noreply.github.com>
2024-03-07 15:53:58 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-var languages = ['English', 'Urdu', 'Chinese', 'Italian', 'French', 'Arabic'];
-console.log("List of languages:");
-for (var _i = 0, languages_1 = languages; _i < languages_1.length; _i++) {
-    var top_1 = languages_1[_i];
-    console.log(top_1);
-}
+var book = {
+    name: 'ESSENTIAL TYPESCRIPT',
+    price: 400
+};
+var apple = {
+    name: 'apple',
+    price: 360
+};
+console.log("book name: ".concat(book.name, ", price: $").concat(book.price));
+console.log("apple name: ".concat(apple.name, ", price: $").concat(apple.price));
--- app.ts
+++ app.ts
@@ -1,6 +1,15 @@
-// Exercise 20 
-let languages : string[] = ['English','Urdu','Chinese','Italian','French','Arabic']
-console.log("List of languages:")
-for(let top of languages){
-    console.log(top)
+// Exercise 21
+interface item {
+    name : string
+    price : number
 }
+const book : item ={
+    name : 'ESSENTIAL TYPESCRIPT'
+    price : 400 
+}
+const apple : item ={
+    name : 'apple'
+    price : 360
+}
+console.log(`book name: ${book.name}, price: $${book.price}`)
+console.log(`apple name: ${apple.name}, price: $${apple.price}`)
