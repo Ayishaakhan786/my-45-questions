29cf4df419d4f0e06a599bb6c7c874d334b5e375
Ayishaakhan786 <160420762+Ayishaakhan786@users.noreply.github.com>
2024-03-13 01:33:40 -0700

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,16 @@
+// Exercise 25
+// First condition
+var alien_color = "Green";
+// here we apply the First condition which is the loop statement so,
+//if(alien_color == "Green"){
+//   console.log("The player just earned only 5 points")
+//}
+// if we give the color "Green in input so we get that the above line"
+// but if we give the another color in input so the output will become empty
+// Second condition
+alien_color = "Red";
+if (alien_color == "Green") {
+    console.log("The player just earned only 5 points");
+}
+// so, in Second condition whenever we pass the another value of the variable so it will give us empty output
+// now we can see the both conditions.  
