b6f5089271caf4014244c4b05268d9a32b4d12b9
Ayishaakhan786 <160420762+Ayishaakhan786@users.noreply.github.com>
2024-03-14 14:06:13 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,47 +1,26 @@
-// Exercise 27
-var alien_color = "Green";
-// if the alien color is green then print a statement that the player earn 5 points.
-if (alien_color == "Green") {
-    console.log("The player just earned only 5 points for shooting the alien.");
+// Exercise 28 "Stages of life."
+var age = 18;
+// if the age is less than 2 then print a message that the person is a baby.
+if (age < 2) {
+    console.log("The Person is a baby.");
 }
-// if the alien color is yellow then print a statement that the player earn 10 points.
-else if (alien_color == "yellow") {
-    console.log("The player just earned only 10 points for shooting the alien.");
+// if the age is at least 2 years old but less than 4 then print message that the person is a toddler.
+else if (age < 2 && age < 4) {
+    console.log("The Person is a toddler.");
 }
-// if the alien color is red then print a statement that the player earn 15 points.
-else if (alien_color == "red") {
-    console.log("The player just earned only 15 points for shooting the alien.");
+// if the age is at least 4 years old but less than 13 then print message that the person is a kid.
+else if (age < 4 && age < 13) {
+    console.log("The Person is a kid.");
 }
+// if the age is at least 13 years old but less than 20 then print message that the person is a teenager.
+else if (age < 13 && age < 20) {
+    console.log("The Person is a teenager.");
+}
+// if the age is at least 20 years old but less than 65 then print message that the person is a adult.
+else if (age < 20 && age < 65) {
+    console.log("The Person is a adult.");
+}
+// if the age is at least 65 year or older than print a message that the person is an alder.
 else {
-    console.log("pase select right color.");
-}
-// write three versions of this program make sure that each message is printed for the
-// appropiate color alien.
-// version 2
-alien_color = "Red";
-if (alien_color == "Green") {
-    console.log("The player just earned only 5 points for shooting the alien.");
-}
-else if (alien_color == "yellow") {
-    console.log("The player just earned only 10 points for shooting the alien.");
-}
-else if (alien_color == "red") {
-    console.log("The player just earned only 15 points for shooting the alien.");
-}
-else {
-    console.log("pase select right color.");
-}
-// version 3
-alien_color = "yellow";
-if (alien_color == "Green") {
-    console.log("The player just earned only 5 points for shooting the alien.");
-}
-else if (alien_color == "yellow") {
-    console.log("The player just earned only 10 points for shooting the alien.");
-}
-else if (alien_color == "red") {
-    console.log("The player just earned only 15 points for shooting the alien.");
-}
-else {
-    console.log("pase select right color.");
+    console.log("Person is an alder");
 }
