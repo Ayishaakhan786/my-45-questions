f304e15877091b17784795e3ca9eabb2a62ac0c8
Ayisha Khan <160420762+Ayishaakhan786@users.noreply.github.com>
2024-03-22 20:15:01 -0700

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,26 +1,17 @@
-// Exercise 28 "Stages of life."
-let age: number = 18;
-// if the age is less than 2 then print a message that the person is a baby.
-if(age<2){
-    console.log("The Person is a baby.")
-}
-// if the age is at least 2 years old but less than 4 then print message that the person is a toddler.
-else if(age<2 && age <4){
-    console.log("The Person is a toddler.")
-}
-// if the age is at least 4 years old but less than 13 then print message that the person is a kid.
-else if(age<4 && age <13){
-    console.log("The Person is a kid.")
-}
-// if the age is at least 13 years old but less than 20 then print message that the person is a teenager.
-else if(age<13 && age <20){
-    console.log("The Person is a teenager.")
-}
-// if the age is at least 20 years old but less than 65 then print message that the person is a adult.
-else if(age<20 && age <65){
-    console.log("The Person is a adult.")
-}
-// if the age is at least 65 year or older than print a message that the person is an alder.
-else{
-    console.log("Person is an alder")
-}
+// Exercise 32 "Chacking Usernames."
+// Array of current users.
+let current_users: string[] = ['Ayisha', 'fatima', 'Sameer', 'Bismah', 'Minahil'];
+// Array of new users.
+let new_users: string[] = ['Sahar', 'Ayisha', 'Sameer', 'Rida', 'Jannat'];
+// loop through new_users to chack for usernames availability.
+new_users.forEach(one_new_user =>{
+    // Making a condition for Username already exists & save in our_condition variable.
+    let our_conditions = current_users.some(one_new_user => one_new_user.toLowerCase() === one_new_user.toLowerCase())
+    // print Different Messages by using if-Else statements.
+    if(our_conditions){
+        console.log(`sorry ${one_new_user} is already taken`);
+    }
+    else{
+        console.log(`This Username ${one_new_user} is available`);
+    }
+}) 
