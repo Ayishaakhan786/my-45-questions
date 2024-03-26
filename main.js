3f1683fe30f4cce90adb280631fb42646ffc89a2
Ayisha Khan <160420762+Ayishaakhan786@users.noreply.github.com>
2024-03-25 19:01:10 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-// Exercise: 36 "T-shirt".
+// Exercise 37 "large shirts"
+// making a function.
 function make_shirt(size, printMessage) {
-    console.log("You selected ".concat(size, " size shirt with ").concat(printMessage, " printed on shirt"));
+    if (size === void 0) { size = "large"; }
+    if (printMessage === void 0) { printMessage = "I Love Typescript"; }
+    console.log("creating a ".concat(size, " with the ").concat(printMessage, " printed on the shirt"));
 }
-make_shirt("Meduim", "Ayisha khan");
+// Now calling a function with by default values.
+make_shirt();
+// now we just change the size of the shirt but the message remains same.
+make_shirt("Meduim");
+// Now we change the message but the is same or any. (small)
+make_shirt("small", "I Love HTML & Javascript");
--- main.ts
+++ main.ts
@@ -1,13 +1,11 @@
-// Exercise: 36 "T-shirt".
-function makeShirt (size: string, printMessage: string){
-    console.log(`You selected ${size} size shirt with ${printMessage} printed on shirt`)
+// Exercise 37 "large shirts"
+// making a function.
+function make_shirt(size: string = "large", printMessage: string = "I Love Typescript"){
+    console.log(`creating a ${size} with the ${printMessage} printed on the shirt`)
 }
-makeShirt("Meduim", "Ayisha khan")
-
-// Now make some changes in this code. 
-// first, we copy the code make some minar changes in it.
-
-function make_Shirt (size: string, printMessage: string){
-    console.log(`You selected ${size} size shirt with ${printMessage} printed on shirt`)
-}
-make_Shirt("Large", "Karachi")
+// Now calling a function with by default values.
+make_shirt();
+// now we just change the size of the shirt but the message remains same.
+make_shirt("Meduim");
+// Now we change the message but the is same or any. (small)
+make_shirt("small", "I Love HTML & Javascript");
