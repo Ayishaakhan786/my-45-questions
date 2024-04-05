30a978844c2b10993b612c7adfcc8de345968158
Ayisha Khan <160420762+Ayishaakhan786@users.noreply.github.com>
2024-04-05 14:27:17 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-// Exercise: 41 "Megicians"
-// first we make an array in which we put some values & define the function to print 
-// each megician name from array.
-var magicians = ["Saamri", "Subhan", "Herrypotter"];
+// Exercise: 43 "unchanged Magicians."
+// Define the function to show magician names.
 function show_magicians(magicians) {
-    magicians.forEach(function (magicians) {
-        console.log(magicians);
-    });
+    magicians.forEach(function (name) { return console.log(name); });
 }
-show_magicians(magicians);
+// Function to make magicians great through .map() it will modify array.
+function make_great(magicians) {
+    return magicians.map(function (name) { return "The Great ".concat(name); });
+}
+// Define an array of magicians names
+var magicians_names = ["sameer", "Ayisha", "Erum"];
+// call make_great function to modify magician names.
+var great_magicians = make_great(magicians_names);
+// call show magicians that show modified lis of magicians.
+//show_magicians(great_magicians);
+// Make a copy of original array thorugh .slice() function.
+var copy_magician_names = magicians_names.slice();
+// Modify the copied array to include "The Great" with their names.
+var copy_great_magicians = make_great(copy_magician_names);
+// Now show both arrays(original & copied).
+// "ORIGINAL"
+show_magicians(great_magicians);
+// "COPIED"
+show_magicians(copy_magician_names);
--- main.ts
+++ main.ts
@@ -1,10 +1,27 @@
-// Exercise: 41 "Megicians"
-// first we make an array in which we put some values & define the function to print 
-// each megician name from array.
-let magicians: string[] = ["Saamri", "Subhan", "Herrypotter"];
+// Exercise: 43 "unchanged Magicians."
+// Define the function to show magician names.
 function show_magicians(magicians: string[]){
-    magicians.forEach(magicians => {
-        console.log(magicians);
-    });
+    magicians.forEach(name => console.log(name));
 }
-show_magicians(magicians);
+// Function to make magicians great through .map() it will modify array.
+function make_great(magicians: string[]){
+    return magicians.map(name => `The Great ${name}`);
+}
+// Define an array of magicians names
+let magicians_names = ["sameer", "Ayisha", "Erum"];
+
+// call make_great function to modify magician names.
+let great_magicians = make_great(magicians_names);
+// call show magicians that show modified lis of magicians.
+//show_magicians(great_magicians);
+
+// Make a copy of original array thorugh .slice() function.
+let copy_magician_names = magicians_names.slice()
+
+// Modify the copied array to include "The Great" with their names.
+let copy_great_magicians = make_great(copy_magician_names);
+// Now show both arrays(original & copied).
+// "ORIGINAL"
+show_magicians(great_magicians);
+// "COPIED"
+show_magicians(copy_magician_names);
